21538bad8bd6bbae536cefff77a13f1aa5bbba19
Jesus <jesuswrivas@gmail.com>
2022-12-21 18:13:16 -0500

    Grid creation while changing input range value
    Add divs to grid-container while changing input range value
    Create a grid while changing input range value
    Add eventListeners to all divs created inside grid-container. A mouseover
    event was created that changed the backgroundcolor (to red, as a test)

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let range = document.querySelector(".mod-buttons-range")
 let rangeValue = document.querySelector(".mod-buttons-rangevalue")
+let gridContainer = document.querySelector(".grid-container")
 
 
 /*Event for changing the span Innertext Value*/
@@ -8,4 +9,36 @@ range.addEventListener("change", (e) => {
 })
 
 
-/*Event for creating the grid*/
+/*Event for creating the grid when the input changes*/
+
+range.addEventListener("change", (e) => {
+
+    while (gridContainer.firstChild) {
+        gridContainer.firstChild.remove()
+    }
+    for (let i = 1; i <= range.value * range.value; i++) {
+        let newDiv = document.createElement("div")
+        gridContainer.append(newDiv)
+    }
+    gridContainer.style.gridTemplateColumns = `repeat(${range.value}, 1fr)`
+    gridContainer.style.gridTemplateRows = "auto"
+
+    /*Event to color the divs inside grid container */
+    let divsVar = document.querySelectorAll(".grid-container div")
+    console.dir(divsVar)
+
+    for (let div of divsVar) {
+        console.dir(div)
+        div.addEventListener("mouseover", (e) => {
+            div.style.backgroundColor = "red"
+        })
+    }
+})
+
+
+
+
+
+
+
+
