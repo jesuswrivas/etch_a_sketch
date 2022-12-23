668ffefedf4d797fade88b6b734fe5d41d799651
Jesus <jesuswrivas@gmail.com>
2022-12-22 20:56:59 -0500

    Finished Etch a Sketch project TOP
    Create clear button. It will refresh the page when clicked
    Create color button. It will use the color selected on the color input
    as a background color on the events of the div inside the grid
    Create rainbow button. The color used in the event will be random

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 let range = document.querySelector(".mod-buttons-range")
 let rangeValue = document.querySelector(".mod-buttons-rangevalue")
 let gridContainer = document.querySelector(".grid-container")
+let colorPalette = document.querySelector(".mod-buttons-palette")
+
+
+
+/* First values and creating the grid according to initial value on range input */
+rangeValue.innerHTML = `${range.value}x${range.value}`
+
+for (let i = 1; i <= range.value * range.value; i++) {
+    let newDiv = document.createElement("div")
+    gridContainer.append(newDiv)
+}
+gridContainer.style.gridTemplateColumns = `repeat(${range.value}, 1fr)`
+gridContainer.style.gridTemplateRows = "auto"
+
+let divsVar = document.querySelectorAll(".grid-container div")
+
+for (let div of divsVar) {
+    div.addEventListener("mouseover", (e) => {
+        div.style.backgroundColor = colorPalette.value
+    })
+}
+
+
 
 
 /*Event for changing the span Innertext Value*/
@@ -25,18 +48,66 @@ range.addEventListener("change", (e) => {
 
     /*Event to color the divs inside grid container */
     let divsVar = document.querySelectorAll(".grid-container div")
-    console.dir(divsVar)
 
     for (let div of divsVar) {
-        console.dir(div)
         div.addEventListener("mouseover", (e) => {
-            div.style.backgroundColor = "red"
+            div.style.backgroundColor = colorPalette.value
         })
     }
 })
 
+/* Event on Clear button for deleting the grid*/
+
+let clearButton = document.querySelector("#clear_button")
+clearButton.addEventListener("click", (e) => {
+    document.location.reload()
+})
+
+/* Event on Color button*/
 
 
+let colorButton = document.querySelector("#color_button")
+colorButton.addEventListener("click", (e) => {
+
+
+    let divsVar = document.querySelectorAll(".grid-container div")
+    for (let div of divsVar) {
+
+        div.addEventListener("mouseover", (e) => {
+            div.style.backgroundColor = colorPalette.value
+        })
+    }
+
+}, { once: true })
+
+
+
+
+/* Event on rainbow mode to change randomly the color of the hovering*/
+let rainbowButton = document.querySelector("#rainbow_button")
+rainbowButton.addEventListener("click", (e) => {
+
+    let divsVar = document.querySelectorAll(".grid-container div")
+    for (let div of divsVar) {
+        div.addEventListener("mouseover", (e) => {
+            div.style.backgroundColor = randomColor()
+        })
+    }
+
+}, { once: true })
+
+
+
+
+/* Auxiliary functions */
+
+randomColor = function () {
+    let randomR = Math.floor(Math.random() * 255 + 1)
+    let randomG = Math.floor(Math.random() * 255 + 1)
+    let randomB = Math.floor(Math.random() * 255 + 1)
+    return `rgb(${randomR},${randomG},${randomB})`
+
+}
 
 
 
